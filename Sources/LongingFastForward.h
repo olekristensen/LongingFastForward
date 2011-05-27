de715019786e149f308731d48e55c8d3aede90f9
Ole Kristensen <ole@kristensen.name>
2011-05-27 20:46:20 +0200

    -a

---
+++ Sources/LongingFastForward.h
@@ -0,0 +1,21 @@
+/*
+ *  LongingFastForward.h
+ *  LongingFastForward
+ *
+ *  Created by ole kristensen on 27/05/11.
+ *  Copyright 2011 Recoil Performance Group. All rights reserved.
+ *
+ */
+
+#import <LongingFastForward/LFFCameraAngle.h>
+#import <LongingFastForward/LFFCameraDevice.h>
+#import <LongingFastForward/LFFCameraProfile.h>
+#import <LongingFastForward/LFFCameraSettings.h>
+#import <LongingFastForward/LFFCameraStats.h>
+#import <LongingFastForward/LFFCaptureClient.h>
+#import <LongingFastForward/LFFCaptureSession.h>
+#import <LongingFastForward/LFFEvent.h>
+#import <LongingFastForward/LFFImageHDR.h>
+#import <LongingFastForward/LFFImageLDR.h>
+#import <LongingFastForward/LFFLoggingEntity.h>
+#import <LongingFastForward/LFFResearchStation.h>
